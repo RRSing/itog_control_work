6867ee561807f7541eee2030d1b39a88bd77de9d
Renat Singatullin <ren-sing@yandex.ru>
2024-05-24 10:37:05 +0300

    Добавлена функция по проверке значений массивов

--- Program.cs
+++ Program.cs
@@ -15,3 +15,31 @@ void showArray(string[] toShow) {
     Console.WriteLine();
 }
 
+int step1 = showInvitation();
+
+if(step1 == 1) {
+    Console.WriteLine("Вы выбрали работу с готовыми массивами");
+    Console.WriteLine("1 - [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 - [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 - [“Russia”, “Denmark”, “Kazan”]");
+    Console.WriteLine("Выберите вариант от 1 до 3х");
+    int step2 = Convert.ToInt32(Console.ReadLine());
+    if(step2 == 1) {string[] text = ["Hello", "2", "world", ":-)"]; showArray(text);}
+    if(step2 == 2) {string[] text = ["1234", "1567", "-2", "computer science"]; showArray(text);}
+    if(step2 == 2) {string[] text = ["Russia", "Denmark", "Kazan"]; showArray(text);}
+}
+if(step1 == 2) {
+    string[] text = new string[4];
+    Console.WriteLine("Введите первый элемент массива");
+    text[0] = Console.ReadLine();
+    Console.WriteLine("Введите второй элемент массива");
+    text[1] = Console.ReadLine();
+    Console.WriteLine("Введите третий элемент массива");
+    text[2] = Console.ReadLine();
+    Console.WriteLine("Введите четвертый элемент массива");
+    text[3] = Console.ReadLine();
+    showArray(text);
+}
+if(step1 != 1 && step1 != 2) {
+    showInvitation();
+}
